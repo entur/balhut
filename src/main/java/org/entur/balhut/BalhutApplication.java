d501e6a554e0434482584dcf8ef1f7482deb968f
mansoor.sajjad <sh.mansoor.com@gmail.com>
2023-01-11 19:29:35 +0100

    Camel free balhut.

--- src/main/java/org/entur/balhut/BalhutApplication.java
+++ src/main/java/org/entur/balhut/BalhutApplication.java
@@ -1,20 +1,48 @@
 package org.entur.balhut;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.task.configuration.EnableTask;
-import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
+
+import java.io.InputStream;
+import java.util.stream.Stream;
 
 @SpringBootApplication
-@EnableTask
-public class BalhutApplication {
+@EnableRetry
+public class BalhutApplication implements ApplicationRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(BalhutApplication.class);
+
+    private final BalhutService bs;
+
+    public BalhutApplication(BalhutService bs) {
+        this.bs = bs;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BalhutApplication.class, args);
     }
 
-    @Bean
-    public BalhutTaskListener taskExecutionListener() {
-        return new BalhutTaskListener();
+    @Override
+    public void run(ApplicationArguments args) {
+        logger.info("Started running balhut service");
+        Stream.of(bs.loadAddressesFile())
+                .map(bs::unzipAddressesFileToWorkingDirectory)
+                .map(bs::readKartverketAddressesFromFile)
+                .map(bs::createPeliasDocumentsForAllIndividualAddresses)
+                .map(bs::addPeliasDocumentStreamForStreets)
+                .map(bs::createCSVFile)
+                .findFirst()
+                .ifPresentOrElse(inputStream -> {
+                    String outputFilename = bs.getOutputFilename();
+                    InputStream csvZipFile = bs.zipCSVFile(inputStream, outputFilename);
+                    bs.uploadCSVFile(csvZipFile, outputFilename);
+                    bs.copyCSVFileAsLatestToConfiguredBucket(outputFilename);
+                    logger.info("Finished running balhut service");
+                }, () -> logger.info("No or empty addresses file found"));
     }
 }
---
+++ src/main/java/org/entur/balhut/BalhutService.java
@@ -0,0 +1,116 @@
+package org.entur.balhut;
+
+import org.entur.balhut.addresses.PeliasDocumentMapper;
+import org.entur.balhut.addresses.StreetMapper;
+import org.entur.balhut.addresses.kartverket.KartverketAddress;
+import org.entur.balhut.addresses.kartverket.KartverketAddressReader;
+import org.entur.balhut.blobStore.BalhutBlobStoreService;
+import org.entur.balhut.blobStore.KakkaBlobStoreService;
+import org.entur.geocoder.Utilities;
+import org.entur.geocoder.ZipUtilities;
+import org.entur.geocoder.csv.CSVCreator;
+import org.entur.geocoder.model.PeliasDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Service
+public class BalhutService {
+
+    private static final Logger logger = LoggerFactory.getLogger(BalhutService.class);
+
+    @Value("${blobstore.gcs.kakka.kartverket.addresses.folder:kartverket/addresses}")
+    private String kartverketAddressesFolder;
+
+    @Value("${balhut.workdir:/tmp/balhut/geocoder}")
+    private String balhutWorkDir;
+
+    private final KakkaBlobStoreService kakkaBlobStoreService;
+    private final BalhutBlobStoreService balhutBlobStoreService;
+    private final PeliasDocumentMapper addressMapper;
+    private final StreetMapper streetMapper;
+
+    public BalhutService(KakkaBlobStoreService kakkaBlobStoreService,
+                         BalhutBlobStoreService balhutBlobStoreService,
+                         PeliasDocumentMapper addressMapper,
+                         StreetMapper streetMapper) {
+        this.kakkaBlobStoreService = kakkaBlobStoreService;
+        this.balhutBlobStoreService = balhutBlobStoreService;
+        this.addressMapper = addressMapper;
+        this.streetMapper = streetMapper;
+    }
+
+    protected InputStream loadAddressesFile() {
+        logger.info("Loading addresses file");
+        return kakkaBlobStoreService.findLatestBlob(kartverketAddressesFolder);
+    }
+
+    protected Path unzipAddressesFileToWorkingDirectory(InputStream inputStream) {
+        logger.info("Unzipping addresses file");
+        ZipUtilities.unzipFile(inputStream, balhutWorkDir + "/addresses");
+        try (Stream<Path> paths = Files.walk(Paths.get(balhutWorkDir + "/addresses"))) {
+            return paths
+                    .filter(Utilities::isValidFile)
+                    .findFirst()
+                    .orElseThrow(() -> new RuntimeException("Unzipped file not found."));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected Stream<KartverketAddress> readKartverketAddressesFromFile(Path path) {
+        logger.info("Read kartverket addresses file");
+        return KartverketAddressReader.read(path);
+    }
+
+    protected List<PeliasDocument> createPeliasDocumentsForAllIndividualAddresses(Stream<KartverketAddress> kartverketAddresses) {
+        logger.info("Converting stream of kartverket addresses to pelias documents.");
+
+        // Create documents for all individual addresses
+        return kartverketAddresses
+                .parallel()
+                .map(addressMapper::toPeliasDocument)
+                .toList();
+    }
+
+    protected Stream<PeliasDocument> addPeliasDocumentStreamForStreets(List<PeliasDocument> individualAddressDocuments) {
+        logger.info("Adding peliasDocuments stream for unique streets");
+
+        // Create separate document per unique street
+        return Stream.concat(
+                individualAddressDocuments.stream(),
+                streetMapper.createStreetPeliasDocumentsFromAddresses(individualAddressDocuments));
+    }
+
+    protected InputStream createCSVFile(Stream<PeliasDocument> peliasDocuments) {
+        logger.info("Creating CSV file form PeliasDocuments stream");
+        return CSVCreator.create(peliasDocuments);
+    }
+
+    protected String getOutputFilename() {
+        return "balhut_export_geocoder_" + System.currentTimeMillis();
+    }
+
+    protected InputStream zipCSVFile(InputStream inputStream, String filename) {
+        logger.info("Zipping the created csv file");
+        return ZipUtilities.zipFile(inputStream, filename + ".csv");
+    }
+
+    protected void uploadCSVFile(InputStream csvZipFile, String filename) {
+        logger.info("Uploading the CSV file");
+        balhutBlobStoreService.uploadBlob(filename + ".zip", csvZipFile);
+    }
+
+    protected void copyCSVFileAsLatestToConfiguredBucket(String filename) {
+        logger.info("Coping latest file to haya");
+        balhutBlobStoreService.copyBlobAsLatestToTargetBucket(filename + ".zip");
+    }
+}
